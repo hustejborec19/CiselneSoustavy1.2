aa4917aa8de424923a8892d1a9e9350f55d2f8d9
hustejborec19 <58736308+hustejborec19@users.noreply.github.com>
2019-12-17 20:40:11 +0100

    Add files via upload

---
+++ CiselneSoustavyTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CiselneSoustavyTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CiselneSoustavyTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bfe410e6-c6cb-484e-8c88-be33d01ccf2e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CiselneSoustavyTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CiselneSoustavy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CiselneSoustavyTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int cislo = 15;
+            Prevod prevod = new Prevod(cislo);
+            string vysledek1 = "f";
+            string v = prevod.DecToHexa();
+            Assert.AreEqual(v, vysledek1);
+        }
+    }
+}
